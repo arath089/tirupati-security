5b9ef6d713f95317ad96e67ccdd6aea5e89fb42d
Archit Rathi <architrathi@Archits-MacBook-Pro-2.local>
2025-07-16 00:43:51 -0700

    feat: Add main and app.jsx file

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Navigation from "./components/Navigation";
+import Hero from "./components/Hero";
+import Testimonials from "./components/Testimonials";
+import Services from "./components/Services";
+import Partners from "./components/Partners";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Licensing from "./components/Licensing";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="min-h-screen">
+      <Navigation />
+      <Hero />
+      <Testimonials />
+      <Services />
+      <Partners />
+      <About />
+      <Contact />
+      <Licensing />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
